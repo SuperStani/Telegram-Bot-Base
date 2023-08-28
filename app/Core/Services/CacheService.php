4aaceb0ddd713cbd72977c2ac28d25d2da2893e7
Stanislav Zatic <s.zatic@unifun.com>
2023-08-28 14:20:00 +0300

    upgrading cache service

--- app/Core/Services/CacheService.php
+++ app/Core/Services/CacheService.php
@@ -21,25 +21,6 @@ class CacheService
         $this->logger = $logger;
     }
 
-    public function setUserPage(int $user_id, string|int $page)
-    {
-        try {
-            $this->redisController->setKey("PAGE_" . $user_id, $page, "60");
-        } catch (Exception $e) {
-            $this->logger->warning("SetUserPageCache failed!", $e->getMessage());
-        }
-    }
-
-    public function getUserPage(int $user_id): string|bool
-    {
-        try {
-            return $this->redisController->getKey("PAGE_" . $user_id);
-        } catch (Exception $e) {
-            $this->logger->warning("GetUserPageCache failed!", $e->getMessage());
-            return false;
-        }
-    }
-
     public function setKey(string $key, string $value, int $expire_time = null)
     {
         try {
